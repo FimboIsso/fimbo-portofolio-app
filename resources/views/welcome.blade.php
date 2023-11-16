057365ec83dddf3a65509e81a6e517529fd72b8c
Fimbo Isso <fimbo.isso@gmail.com>
2023-11-16 12:28:48 +0200

    design enhachement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,7 +97,7 @@
                         <div class="col-lg-6">
                             <ul>
                                 <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>1 Febraury 1995</span></li>
-                                <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.uzashop.com</span></li>
+                                <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong> <span>www.uzashop.co</span></li>
                                 <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>+243 997 643 592</span></li>
                                 <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Beni, North-Kivu, Congo DR</span></li>
                             </ul>
@@ -572,7 +572,7 @@
                             <p>+243 997 643 592</p>
                         </div>
 
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
+{{--                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>--}}
 {{--                        <iframe src="https://www.google.com/maps/place/Beni,+Democratic+Republic+of+the+Congo/@0.4984969,29.4713123,17.71z/data=!4m6!3m5!1s0x1761ab017951ba17:0xa45247d1f6b2a53c!8m2!3d0.4990124!4d29.4527538!16s%2Fg%2F122cz20k?hl=en-GB&entry=ttu" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>--}}
                     </div>
 
